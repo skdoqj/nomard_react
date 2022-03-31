c8bbaefd24b6fbe9ff3c1e93ae8103ebace6e650
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-03-31 19:12:14 +0900

    6.4

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
 import Button from "./Button";
 import { useState, useEffect } from "react"
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []); 
+  return <h1>hello</h1>
+}
+
 function App() {
-  const [count , setCount] = useState(0);
-  const onClick = () => setCount((prev) => prev + 1);
-  // console.log("I run every time")
-
-  const [key, setKey] = useState("");
-  const onChange = (event) => setKey(event.target.value);
-    useEffect(() => {
-      console.log("I run only once")
-    }, []);
-
-    useEffect(()=> {
-        console.log("I run when 'key' changes")
-    },[key])
-  
-    useEffect(()=> {
-      console.log("I run when 'count' changes")
-    },[count])
-    
-    useEffect(()=> {
-      console.log("I run when 'key' & 'count' change")
-    },[key, count])
-  
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev); 
 
   return (
     <div >
-      <input 
-        value={key}
-        onChange={onChange}
-        type="text"
-        placeholder="search" 
-      />
-      <h1>{count}</h1>
-      <button onClick={onClick}>버튼</button>
-      {/* <Button text={"hey"}/> */}
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
     </div>
   );
 }
