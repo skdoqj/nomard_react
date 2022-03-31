6b21658a76e7437d047bede58229bd4373391639
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-03-31 16:19:20 +0900

    6.3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import Button from "./Button";
+import { useState, useEffect } from "react"
 
 function App() {
+  const [count , setCount] = useState(0);
+  const onClick = () => setCount((prev) => prev + 1);
+  // console.log("I run every time")
+
+  const [key, setKey] = useState("");
+  const onChange = (event) => setKey(event.target.value);
+    useEffect(() => {
+      console.log("I run only once")
+    }, []);
+
+    useEffect(()=> {
+        console.log("I run when 'key' changes")
+    },[key])
+  
+    useEffect(()=> {
+      console.log("I run when 'count' changes")
+    },[count])
+    
+    useEffect(()=> {
+      console.log("I run when 'key' & 'count' change")
+    },[key, count])
+  
+
   return (
     <div >
-      <h1>Welcome</h1>
-      <Button text={"hey"}/>
+      <input 
+        value={key}
+        onChange={onChange}
+        type="text"
+        placeholder="search" 
+      />
+      <h1>{count}</h1>
+      <button onClick={onClick}>버튼</button>
+      {/* <Button text={"hey"}/> */}
     </div>
   );
 }
