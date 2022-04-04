150c9a5209685a3da3e5f01225941abf8c84a3ec
skdoqj <80323356+skdoqj@users.noreply.github.com>
2022-04-04 21:06:28 +0900

    7.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import Button from "./Button";
 import { useState, useEffect } from "react"
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []); 
-  return <h1>hello</h1>
-}
+ 
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev); 
-
+  const [toDo, setToDo] = useState("");
+  //리스트
+  const [toDoss, setToDoss] = useState([]);
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    //서브밋 방지
+    event.preventDefault();
+    // 빈칸일 때
+    if (toDo === "") {
+      return;
+    };
+    //기존 리스트 안에 새로운 항목 추가하기 
+    setToDoss(currentArray => [toDo, ...currentArray]);
+    //창 비우기
+    setToDo("");
+    
+  }
+  console.log(toDoss.map( (item, index) => (
+    <li key={index}>{item}</li>
+  )));
+  
   return (
     <div >
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "hide" : "show"}</button>
+      <h1>My To Do ({toDoss.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write to do"></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDoss.map( (item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
